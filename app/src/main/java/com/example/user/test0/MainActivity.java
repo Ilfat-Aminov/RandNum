926d7f1f6de58be92695213dd44b52478b3a26f8
user <example@example.com>
2017-11-13 13:12:07 +0300

    “Hide_keyboard_added”

--- app/src/main/java/com/example/user/test0/MainActivity.java
+++ app/src/main/java/com/example/user/test0/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.user.test0;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),  InputMethodManager.HIDE_NOT_ALWAYS);
         Random random = new Random();
         try {
             Integer number = Integer.parseInt((editText3.getText().toString()));
